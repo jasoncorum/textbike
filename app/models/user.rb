ada6a5bd80843fc89b3a683fc47a37598128398e
Jason Corum <jason.corum@gmail.com>
2014-07-17 22:41:09 -0400

    Adds validation for phone_number field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 
 	has_many :commutes, inverse_of: :user
 
+	validates :phone,:presence => true,
+            :numericality => true,
+            :length => { :minimum => 10, :maximum => 10 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
