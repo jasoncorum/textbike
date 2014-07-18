ac41fbc628d307cc01b807d5ad8bbaa0dd04dbfa
Jason Corum <jason.corum@gmail.com>
2014-07-17 22:21:33 -0400

    Removes bad regex for phone_number

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ActiveRecord::Base
 
 	has_many :commutes, inverse_of: :user
 
-	validates :phone_number, format: { with: ^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$
-, message: "Please enter your \"Cell phone\" in this format: 5555555555" }
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
